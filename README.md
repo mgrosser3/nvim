257d07ae12577d89a2423a84071c4e07eed2b811
Martin Großer <grosser.martin@hotmail.de>
2023-10-21 19:47:43 +0200

    feat: lsp-zero with prime's config

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Neovim Configuration
 
+## Plugins
+
+- rose-pine
+- lualine
+- treesitter
+- lsp-zero
+
 ## Windows
 - XDG_CONFIG_HOME = ~\.config
 - Treesitter -> cc compiler -> scoop (see https://scoop.sh/) -> gcc
--- init.lua
+++ init.lua
@@ -54,5 +54,15 @@ require("lazy").setup({
 		'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
 	},
 
+	-- Language Server Protocol (LSP) Setup
+	-- VonHeikemen/lsp-zero.nvim
+	{'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+	{'neovim/nvim-lspconfig'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'hrsh7th/nvim-cmp'},
+	{'L3MON4D3/LuaSnip'},
+
 })
 
