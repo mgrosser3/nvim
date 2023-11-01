acb0c62384ac374c70f669ec47dd1883807e0275
Martin Großer <grosser.martin@hotmail.de>
2023-11-01 19:08:29 +0100

    fix: typo in orgmode config

--- after/plugin/orgmode.lua
+++ after/plugin/orgmode.lua
@@ -4,5 +4,5 @@ require('orgmode').setup_ts_grammar()
 -- Setup orgmode
 require('orgmode').setup({
 	org_agenda_files = '~/.org/**/*',
-	org_default_notes_files = '~/.org/refile.org'
+	org_default_notes_file = '~/.org/refile.org'
 })
