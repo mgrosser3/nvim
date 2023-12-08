e0da3c6fd71eb722312ae96c6409aeba58efe087
Martin Großer <grosser.martin@hotmail.de>
2023-12-08 16:47:54 +0100

    fix: dap-ui config and dap keymaps are added

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -5,6 +5,7 @@ return {
 
 		config = function()
 			-- HACK: Hard coded python interpreter!
+			-- INFO: .setup(os.getenv("VIRTUAL_ENV") .. "/bin/python")
 			require('dap-python').setup('~/Entwicklung/.venv/Scripts/python.exe')
 		end
 
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -6,17 +6,10 @@ return {
 		config = function()
 			local dap = require('dap')
 
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-			  dapui.close()
-			end
-
-			dap.listeners.before.event_exited["dapui_config"] = function()
-			  dapui.close()
-			end
+			vim.keymap.set('n', '<F8>', dap.toggle_breakpoint, {})
+			vim.keymap.set('n', '<F9>', dap.continue, {})
+			vim.keymap.set('n', '<F10>', dap.step_over, {})
+			vim.keymap.set('n', '<F11>', dap.step_into, {})
 		end
 	}
 
