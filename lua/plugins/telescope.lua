3f4f955edefdba6e0f1ce081055291e7817abc6e
Martin Großer <grosser.martin@hotmail.de>
2023-12-11 14:02:43 +0100

    feat: add keymaps for navigation in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,44 @@
 return {
 
-	{
-		'nvim-telescope/telescope.nvim', branch = "0.1.x",
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = "0.1.x",
 
-		dependencies = {'nvim-lua/plenary.nvim'},
+    dependencies = { 'nvim-lua/plenary.nvim' },
 
-		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-		end
-		
-	}
+    config = function()
+      local telescope = require('telescope')
+      local builtin = require('telescope.builtin')
+      local actions = require('telescope.actions')
+
+      --
+      -- Telescope Setup
+      --
+
+      telescope.setup({
+        defaults = {
+
+          -- key maps 
+          mappings = {
+            i = { -- insert mode
+              ["<C-k>"] = actions.move_selection_previous, -- move to prev 
+              ["<C-j>"] = actions.move_selection_next,     -- move to next  
+            }
+          }
+
+        } -- defaults
+      })
+
+      --
+      -- Key Bindings
+      --
+
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+
+  }
+
 }
