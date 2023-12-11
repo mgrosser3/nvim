81223d0728a4aa9d798feea334eb73555a11a273
Martin Großer <grosser.martin@hotmail.de>
2023-12-11 11:55:00 +0100

    chore: polish

--- init.lua
+++ init.lua
@@ -12,9 +12,9 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.wo.relativenumber = true
 
 -- Tabstop settings
-vim.opt.tabstop = 2       -- Width of tab character
-vim.opt.softtabstop = 2   -- Amount of white space to be added
-vim.opt.shiftwidth = 2    -- Amount of white space to add in normal mode
+vim.opt.tabstop = 2       -- width of tab character
+vim.opt.softtabstop = 2   -- amount of white space to be added
+vim.opt.shiftwidth = 2    -- amount of white space to add in normal mode
 vim.opt.expandtab = true  -- use spaces instead of tabs
 
 --
@@ -22,7 +22,7 @@ vim.opt.expandtab = true  -- use spaces instead of tabs
 --
 
 if vim.fn.has('win32') then
-	require('windows')
+  require('windows')
 end 
 
 --
@@ -39,12 +39,12 @@ require('gui')
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-  	"git",
-  	"clone",
-  	"--filter=blob:none",
-  	"https://github.com/folke/lazy.nvim.git",
-  	"--branch=stable", -- latest stable release
-  	lazypath,
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
