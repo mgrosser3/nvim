8bd66215c171ad7eddefb4e481c74eb5f591ca57
Martin Großer <grosser.martin@hotmail.de>
2024-05-03 18:22:21 +0200

    feat: removes plugins orgmode
    The syntax highlighting did not work properly, and sometimes it was a bit unstable.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -35,7 +35,7 @@ return {
 					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 					-- Using this option may slow down your editor, and you may see some duplicate highlights.
 					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = {"org"}
+					additional_vim_regex_highlighting = false
 				}
 			}
 		end -- config
