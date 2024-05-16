44ac1c8a4508d28bba2310b29941c12c86839a93
Martin <martin.grosser@sql-ag.de>
2024-05-16 14:44:58 +0200

    feat: adds some keybindings descriptions

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,11 +33,12 @@ return {
       -- Key Bindings
       --
 
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc="files"})
+      vim.keymap.set('n', '<leader>fg', builtin.git_files, {desc="git files"})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc="buffers"})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc="help tags"})
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc="grep string"})
+      vim.keymap.set('n', '<leader>fd', builtin.live_grep, {desc="live grep"})
     end
 
   }
