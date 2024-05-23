470a919eb53b5218de09ba9f6064468ea731718c
Martin Großer <grosser.martin@hotmail.de>
2024-05-23 17:30:27 +0200

    try to configure lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,33 +2,31 @@ return {
 
   --
   -- Language Server Protocol (LSP) Setup
-  -- Collection of functions that will help you setup Neovim's LSP client,
-  -- so you can get IDE-like features with minimum effort.
-  -- https://github.com/VonHeikemen/lsp-zero.nvim
   --
 
   {
+    -- Collection of functions that will help you setup Neovim's LSP client,
+    -- so you can get IDE-like features with minimum effort.
+    -- https://github.com/VonHeikemen/lsp-zero.nvim
     'VonHeikemen/lsp-zero.nvim',
 
     dependencies = {
 
-      -- lspconfig
       -- https://github.com/neovim/nvim-lspconfig
       'neovim/nvim-lspconfig',
 
-      -- mason-lspconfig
       -- https://github.com/williamboman/mason-lspconfig.nvim
       'williamboman/mason-lspconfig.nvim',
 
-      -- cmp
       -- https://github.com/hrsh7th/nvim-cmp
       'hrsh7th/nvim-cmp',
 
-      -- cmp-nvim-lsp
       -- https://github.com/hrsh7th/cmp-nvim-lsp
       'hrsh7th/cmp-nvim-lsp',
 
-      -- LuaSnip
+      -- https://github.com/hrsh7th/cmp-nvim-lua
+      'hrsh7th/cmp-nvim-lua',
+
       -- https://github.com/L3MON4D3/LuaSnip
       'L3MON4D3/LuaSnip',
     },
@@ -36,25 +34,37 @@ return {
     config = function()
       local lsp_zero = require('lsp-zero')
 
+      -- Appearance Settings
+      lsp_zero.set_sign_icons({
+        error = "✘",
+        warn = " ",
+        hint = "",
+        info = "",
+      })
+
+      -- Keybindings
       lsp_zero.on_attach(function(client, bufnr)
-        local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        -- default keybindings
+        -- see :help lsp-zero-keybinding
+        lsp_zero.default_keymaps({ buffer = bufnr })
       end)
 
-      -- Setup mason-lspconfig
+      -- Install and configure Language Server
+      -- with mason, mason-lspconfig and lspconfig.
       require('mason-lspconfig').setup({
-        ensure_installed = { 'vimls', 'lua_ls' },
+
+        -- Language Servers are listed here
+        ensure_installed = { 'vimls', 'lua_ls', 'jdtls', 'pylsp' },
+
         handlers = {
-          lsp_zero.default_setup,
+          -- this is the "default handler"
+          -- it applies to every language server without a "custom handler"
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end,
+
+          -- Configure lua language server for Neovim
+          -- .luarc.json is necessary!
           lua_ls = function()
             local lua_opts = lsp_zero.nvim_lua_ls()
             require('lspconfig').lua_ls.setup(lua_opts)
@@ -62,24 +72,66 @@ return {
         }
       })
 
+      require('lspconfig').jdtls.setup({
+        single_file_support = false,
+        on_attach = function(client, bufnr)
+          print('Hello Java Language Server')
+        end
+      })
+
       -- Setup cmp
+      -- TODO: move this to cmp.lua
       local cmp = require('cmp')
-      local cmp_select = { behavior = cmp.SelectBehavior.Select }
       cmp.setup({
         sources = {
-          { name = 'path' },
           { name = 'nvim_lsp' },
           { name = 'nvim_lua' },
         },
-        formatting = lsp_zero.cmp_format(),
-        mapping = cmp.mapping.preset.insert({
-          ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-          ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+        mapping = {
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-        })
+          ['<ESC>'] = cmp.mapping.abort(),
+          ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+          ['<C-j>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+          ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = 'insert' })
+            else
+              cmp.complete()
+            end
+          end),
+          ['<C-n>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = 'insert' })
+            else
+              cmp.complete()
+            end
+          end),
+        },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        formatting = {
+          -- changing the order of fields so the icon is the first
+          fields = { 'abbr', 'kind', 'menu' },
+
+          -- here is where the change happens
+          format = function(entry, item)
+            local menu_icon = {
+              nvim_lsp = '[nvim_lsp]',
+              luasnip = '[luasnip]',
+              buffer = '[buffer]',
+              path = '[path]',
+              nvim_lua = '[nvim_lua]',
+            }
+
+            item.menu = menu_icon[entry.source.name]
+            return item
+          end,
+        },
+
       })
     end
   },
-
 }
