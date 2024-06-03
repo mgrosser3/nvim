939a4ef49d7f842ed46845affb5c0b083f71e9fb
Martin Großer <grosser.martin@hotmail.de>
2024-06-03 17:05:58 +0200

    chore: polishes telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,17 @@ return {
       local actions = require('telescope.actions')
 
       --
+      -- Key Bindings
+      --
+
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "files" })
+      vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "git files" })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "buffers" })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "help tags" })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "grep string" })
+      vim.keymap.set('n', '<leader>fd', builtin.live_grep, { desc = "live grep" })
+
+      --
       -- Plugin Setup
       --
 
@@ -28,17 +39,6 @@ return {
 
         } -- defaults
       })
-
-      --
-      -- Key Bindings
-      --
-
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "files" })
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "git files" })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "buffers" })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "help tags" })
-      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "grep string" })
-      vim.keymap.set('n', '<leader>fd', builtin.live_grep, { desc = "live grep" })
     end
 
   }
