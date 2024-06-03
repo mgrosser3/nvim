77e3aa7a5ceef92d5073504f752378847c18e08e
Martin Großer <grosser.martin@hotmail.de>
2024-06-03 17:38:25 +0200

    chore: disable providers for remote plugins

--- init.lua
+++ init.lua
@@ -7,6 +7,16 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 --
+-- Disable providers for remote plugins
+-- see https://neovim.io/doc/user/provider.html
+--
+
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+
+--
 -- Neovim Options
 -- :help options
 --
