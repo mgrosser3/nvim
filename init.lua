870db3d76f41b66490ba716d6e2bc7560005bcd3
Martin Großer <grosser.martin@hotmail.de>
2024-08-18 11:48:39 +0200

    feat: disables option autochdir

--- init.lua
+++ init.lua
@@ -45,7 +45,7 @@ vim.opt.undodir = vim.fn.expand("~/.nvim/undo")
 vim.opt.backspace = "indent,eol,start"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-vim.opt.autochdir = true  -- change current working directory whenever you open a file
+vim.opt.autochdir = false -- don't change the working directory automatically
 vim.opt.iskeyword:append("-")
 vim.opt.mouse:append("a") -- enable mouse support for all modes
 vim.opt.clipboard:append("unnamedplus")
