afe002868a8a366eda7f2f0dd3e518b991738fdc
Martin Großer <grosser.martin@hotmail.de>
2024-09-18 20:29:52 +0200

    feat: add keymap <S-CR> for a new line in normal mode

--- init.lua
+++ init.lua
@@ -12,9 +12,9 @@ vim.g.maplocalleader = " "
 --
 
 -- Tabstop
-vim.opt.tabstop = 2 -- width of tab character
-vim.opt.softtabstop = 2 -- amount of white space to be added
-vim.opt.shiftwidth = 2 -- amount of white space to add in normal mode
+vim.opt.tabstop = 2      -- width of tab character
+vim.opt.softtabstop = 2  -- amount of white space to be added
+vim.opt.shiftwidth = 2   -- amount of white space to add in normal mode
 vim.opt.expandtab = true -- use spaces instead of tabs
 
 -- Line wrapping
@@ -28,28 +28,28 @@ vim.opt.smartindent = true -- autoindenting when starting a new line
 
 -- Folding
 vim.o.foldmethod = "syntax" -- use syntax fold method
-vim.o.foldlevelstart = 99 -- open all folds by default
+vim.o.foldlevelstart = 99   -- open all folds by default
 
 -- Search
-vim.opt.incsearch = true -- enable incremental search
+vim.opt.incsearch = true  -- enable incremental search
 vim.opt.ignorecase = true -- ignore case in search pattern
-vim.opt.smartcase = true -- case sensitive search
-vim.opt.hlsearch = false -- disable highlighting
+vim.opt.smartcase = true  -- case sensitive search
+vim.opt.hlsearch = false  -- disable highlighting
 
 -- Appearance
-vim.opt.number = true -- show real line number for current line
+vim.opt.number = true         -- show real line number for current line
 vim.opt.relativenumber = true -- enable relative line numbers
-vim.opt.colorcolumn = "80" -- highlighted line length
-vim.opt.signcolumn = "yes" -- draw the signcolumn (default = "auto")
-vim.opt.cmdheight = 1 -- number of lines to use for the command-line
-vim.opt.scrolloff = 10 -- number of lines to keep above and below the cursor
+vim.opt.colorcolumn = "80"    -- highlighted line length
+vim.opt.signcolumn = "yes"    -- draw the signcolumn (default = "auto")
+vim.opt.cmdheight = 1         -- number of lines to use for the command-line
+vim.opt.scrolloff = 10        -- number of lines to keep above and below the cursor
 vim.opt.completeopt = "menuone,noinsert,noselect"
 
 -- Behaviour
 vim.opt.errorbells = false -- switch off noise in case of errors
-vim.opt.swapfile = false -- disable swapfiles for buffers
-vim.opt.backup = false -- disable backup file
-vim.opt.undofile = true -- enable undo files
+vim.opt.swapfile = false   -- disable swapfiles for buffers
+vim.opt.backup = false     -- disable backup file
+vim.opt.undofile = true    -- enable undo files
 vim.opt.undodir = vim.fn.expand("~/.nvim/undo")
 vim.opt.backspace = "indent,eol,start"
 vim.opt.splitright = true
@@ -72,6 +72,9 @@ vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "netrw" })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- move up
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- move down
 
+-- Add new line in normal mode
+vim.keymap.set("n", "<S-CR>", "o<ESC>")
+
 -- Exit from insert mode by Esc in Terminal
 vim.keymap.set("t", "<esc>", [[<C-\><C-n>]])
 
@@ -85,9 +88,9 @@ vim.keymap.set("i", "<C-c>", "<ESC>") -- CTRL-C -> ESC
 --
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-	pcall(require, "windows")
+  pcall(require, "windows")
 elseif vim.loop.os_uname().sysname == "Linux" then
-	pcall(require, "linux")
+  pcall(require, "linux")
 end
 
 --
@@ -95,7 +98,7 @@ end
 -- ./lua/gui.lua
 --
 if vim.fn.has("gui_running") then
-	pcall(require, "gui")
+  pcall(require, "gui")
 end
 
 --
@@ -105,14 +108,14 @@ end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
