157c698f6e2e6f48f8cc01aba4c930b75e97d37e
Martin Großer <grosser.martin@hotmail.de>
2024-10-03 19:23:30 +0200

    refactor: update lsp plugins and config
    - mason.lua is no longer necessary, the plugin spec and config is moved to lsp.lua
    - the lsp file is inspired by https://lsp-zero.netlify.app/docs/guide/lazy-loading-with-lazy-nvim.html

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,43 +1,74 @@
+--
+-- Language Server Protocol (LSP)
+-- Plugins and Configuration
+--
+
 return {
 
-  --
-  -- Language Server Protocol (LSP) Setup
+  -- https://github.com/VonHeikemen/lsp-zero.nvim
+  -- --------------------------------------------
   -- Collection of functions that will help you setup Neovim's LSP client,
   -- so you can get IDE-like features with minimum effort.
-  -- https://github.com/VonHeikemen/lsp-zero.nvim
-  --
-
   {
     'VonHeikemen/lsp-zero.nvim',
+    branch = 'v4.x',
+    lazy = true,
+    config = false
+  },
+
+  -- https://github.com/williamboman/mason.nvim
+  -- ------------------------------------------
+  -- Plugin 'mason.vim' is a package manager for Neovim
+  -- that runs everywhere Neovim runs. Easily install and
+  -- manage LSP servers, DAP servers, linters, and formatters.
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+
+    config = function()
+      require('mason').setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end
+  },
+
+  -- https://github.com/neovim/nvim-lspconfig
+  -- ----------------------------------------
+  -- The plugin nvim-lspconfig provides defauit LSP client configurations
+  -- for a lot of different LSP servers.
+  {
+    'neovim/nvim-lspconfig',
 
     dependencies = {
 
-      -- lspconfig
-      -- https://github.com/neovim/nvim-lspconfig
-      'neovim/nvim-lspconfig',
+      { 'williamboman/mason.nvim' },
 
-      -- mason-lspconfig
       -- https://github.com/williamboman/mason-lspconfig.nvim
-      'williamboman/mason-lspconfig.nvim',
+      -- ----------------------------------------------------
+      -- The plugin mason-lspconfig closes some gaps between mason.nvim
+      -- (see mason.lua) and nvim-lspconfig.
+      { 'williamboman/mason-lspconfig.nvim' },
 
-      -- cmp
-      -- https://github.com/hrsh7th/nvim-cmp
-      'hrsh7th/nvim-cmp',
-
-      -- cmp-nvim-lsp
       -- https://github.com/hrsh7th/cmp-nvim-lsp
-      'hrsh7th/cmp-nvim-lsp',
+      -- ---------------------------------------
+      -- Completion source for Neovim's built-in language server client.
+      --
+      { 'hrsh7th/cmp-nvim-lsp' },
 
-      -- LuaSnip
-      -- https://github.com/L3MON4D3/LuaSnip
-      'L3MON4D3/LuaSnip',
     },
 
     config = function()
       local lsp_zero = require('lsp-zero')
 
-      lsp_zero.on_attach(function(client, bufnr)
+      local lsp_attach = function(client, bufnr)
         local opts = { buffer = bufnr, remap = false }
+
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -48,36 +79,65 @@ return {
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-      end)
+      end
 
-      -- Setup mason-lspconfig
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'vimls', 'lua_ls' },
-        handlers = {
-          lsp_zero.default_setup,
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
-        }
+      lsp_zero.extend_lspconfig({
+        sign_text = true,
+        lsp_attach = lsp_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
       })
 
-      -- Setup cmp
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'lua_ls' },
+        handlers = {
+          -- the default handler applies to every language server
+          -- without a "custom handler"
+          function(server_name)
+            require('lspconfig')[server_name].setup({})
+          end
+        }
+      })
+    end
+  },
+
+  -- https://github.com/hrsh7th/nvim-cmp
+  -- -----------------------------------
+  -- It provides a completion engine where the completion sources can be
+  -- installed from external repositories (e.g. cmp-nvim-lsp)
+  {
+    'hrsh7th/nvim-cmp',
+
+    event = 'InsertEnter',
+
+    dependencies = {
+      -- https://github.com/L3MON4D3/LuaSnip
+      -- -----------------------------------
+      {
+        'L3MON4D3/LuaSnip',
+        version = "v2.*",
+        -- build = "make install_jsregexp"
+      },
+    },
+
+    config = function()
       local cmp = require('cmp')
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
       cmp.setup({
         sources = {
-          { name = 'path' },
           { name = 'nvim_lsp' },
-          { name = 'nvim_lua' },
         },
-        formatting = lsp_zero.cmp_format(),
         mapping = cmp.mapping.preset.insert({
           ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
           ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
           ['<C-Space>'] = cmp.mapping.complete(),
-        })
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end
+        }
       })
     end
   },
