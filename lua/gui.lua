c4f3c768265fac08b28fc39b94ce69b8755369f8
Martin Großer <martin.grosser@sql-ag.de>
2024-10-15 08:27:05 +0200

    feat: increase line spacing

--- lua/gui.lua
+++ lua/gui.lua
@@ -2,4 +2,7 @@
 -- a graphical interface for Neovim.
 
 -- Use font JetBrains Mono (Nerd Font)
-vim.o.guifont = "JetBrainsMono NF:h11"
+vim.o.guifont = "JetBrainsMono NFM:h11"
+
+-- Space between lines
+vim.opt.linespace = 3
