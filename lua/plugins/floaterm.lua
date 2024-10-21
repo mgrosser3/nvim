6cefb97b3923e7d2bfce0d5123d0ec92b18c5de3
Martin Großer <grosser.martin@hotmail.de>
2024-10-21 20:04:37 +0200

    fix: floaterm remains open
    The floaterm toggle is called after pressing space followed by t.
    It is sometimes a issue if a new word starts with 't'. Double tt
    at the beginning of a word is rather unlikely.

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -2,8 +2,8 @@ return {
   {
     'voldikss/vim-floaterm',
     config = function()
-      vim.keymap.set("n", "<leader>t", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
-      vim.keymap.set("t", "<leader>t", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
+      vim.keymap.set("n", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
+      vim.keymap.set("t", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
     end
   },
 }
