8688f17a8907dda1a54709c12d5107022786c37a
Christian Lenke <christian.lenke.aik@googlemail.com>
2025-01-27 08:54:42 +0100

    feat: back to fzf lua and blink.cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,143 +1,148 @@
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    enabledd = false,
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/nvim-cmp",
-    },
-    config = function()
-      local cmp = require("cmp")
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		enabled = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			local cmp = require("cmp")
 
-      cmp.setup({
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        mapping = {
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-          ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-k>"] = cmp.mapping.select_prev_item(),
-          ["<C-i>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          -- ["<CR>"] = cmp.mapping(
-          --   cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
-          --   { "i", "c" }
-          -- ),
-        },
-        sources = {
-          {
-            name = "nvim_lsp",
-            entry_filter = function(entry, ctx)
-              return cmp.lsp.CompletionItemKind.Text ~= entry:get_kind()
-            end,
-          },
-          -- { name = "buffer" },
-          -- { name = "path" },
-          -- { name = "cmdline" },
-        },
-      })
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				mapping = {
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-i>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					-- ["<CR>"] = cmp.mapping(
+					--   cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+					--   { "i", "c" }
+					-- ),
+				},
+				sources = {
+					{
+						name = "nvim_lsp",
+						entry_filter = function(entry, ctx)
+							return cmp.lsp.CompletionItemKind.Text ~= entry:get_kind()
+						end,
+					},
+					-- { name = "buffer" },
+					-- { name = "path" },
+					-- { name = "cmdline" },
+				},
+			})
 
-      -- Set configuration for specific filetype.
-      cmp.setup.filetype("gitcommit", {
-        sources = cmp.config.sources({
-          { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-        }, {
-          { name = "buffer" },
-        }),
-      })
+			-- Set configuration for specific filetype.
+			cmp.setup.filetype("gitcommit", {
+				sources = cmp.config.sources({
+					{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline({
-          -- ["<CR>"] = cmp.mapping(
-          --   cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
-          --   { "i", "c" }
-          -- ),
-          ['<C-i>'] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
-          ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-          ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        }),
-        sources = {
-          { name = "buffer" },
-        },
-      })
+			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline({
+					-- ["<CR>"] = cmp.mapping(
+					--   cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+					--   { "i", "c" }
+					-- ),
+					["<C-i>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+					["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				}),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
 
-      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-      cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline({
-          -- ["<CR>"] = cmp.mapping(
-          --   cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
-          --   { "i", "c" }
-          -- ),
-          -- ["<C-i>"] = cmp.mapping.complete(),
-          ['<C-i>'] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
-          ["<c-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-          ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-        }),
-        sources = cmp.config.sources({
-          { name = "path" },
-        }, {
-          { name = "cmdline" },
-        }),
-        matching = { disallow_symbol_nonprefix_matching = false },
-      })
-    end,
-  },
-  -- {
-  --   'saghen/blink.cmp',
-  --   lazy = false, -- lazy loading handled internally
-  --   -- optional: provides snippets for the snippet source
-  --   dependencies = 'rafamadriz/friendly-snippets',
-  --
-  --   -- use a release tag to download pre-built binaries
-  --   version = 'v0.*',
-  --   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  --   -- build = 'cargo build --release',
-  --   -- If you use nix, you can build from source using latest nightly rust with:
-  --   -- build = 'nix run .#build-plugin',
-  --
-  --   ---@module 'blink.cmp'
-  --   ---@type blink.cmp.Config
-  --   opts = {
-  --     keymap = {
-  --       show = '<C-space>',
-  --       hide = '<C-e>',
-  --       accept = '<enter>',
-  --
-  --       -- select_and_accept = {},
-  --       select_prev = { '<Up>', '<C-k>' },
-  --       select_next = { '<Down>', '<C-j>' },
-  --
-  --       show_documentation = '<C-space>',
-  --       hide_documentation = '<C-space>',
-  --       scroll_documentation_up = '<C-b>',
-  --       scroll_documentation_down = '<C-f>',
-  --
-  --       snippet_forward = '<Tab>',
-  --       snippet_backward = '<S-Tab>',
-  --     },
-  --
-  --     highlight = {
-  --       -- sets the fallback highlight groups to nvim-cmp's highlight groups
-  --       -- useful for when your theme doesn't support blink.cmp
-  --       -- will be removed in a future release, assuming themes add support
-  --       use_nvim_cmp_as_default = true,
-  --     },
-  --     -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-  --     -- adjusts spacing to ensure icons are aligned
-  --     nerd_font_variant = 'normal',
-  --
-  --     -- experimental auto-brackets support
-  --     -- accept = { auto_brackets = { enabled = true } }
-  --
-  --     -- experimental signature help support
-  --     -- trigger = { signature_help = { enabled = true } }
-  --   }
-  -- }
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline({
+					-- ["<CR>"] = cmp.mapping(
+					--   cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+					--   { "i", "c" }
+					-- ),
+					-- ["<C-i>"] = cmp.mapping.complete(),
+					["<C-i>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "c" }),
+					["<c-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+					["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
+			})
+		end,
+	},
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = "rafamadriz/friendly-snippets",
+
+		-- use a release tag to download pre-built binaries
+		version = "v0.*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' for mappings similar to built-in completion
+			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+			-- see the "default configuration" section below for full documentation on how to define
+			-- your own keymap.
+			keymap = {
+				preset = "enter",
+				["<C-j>"] = { "select_next", "fallback" },
+				["<C-k>"] = { "select_prev", "fallback" },
+				cmdline = {
+					preset = "super-tab",
+					["<C-j>"] = { "select_next", "fallback" },
+					["<C-k>"] = { "select_prev", "fallback" },
+				},
+			},
+
+			appearance = {
+				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- Useful for when your theme doesn't support blink.cmp
+				-- will be removed in a future release
+				use_nvim_cmp_as_default = true,
+				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+
+			-- default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, via `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				-- optionally disable cmdline completions
+				-- cmdline = {},
+			},
+
+			-- experimental signature help support
+			-- signature = { enabled = true }
+		},
+		-- allows extending the providers array elsewhere in your config
+		-- without having to redefine it
+		opts_extend = { "sources.default" },
+	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,84 +1,81 @@
 function my_live_grep()
-  local root = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
-  if vim.v.shell_error == 0 then
-    require("telescope.builtin").live_grep({ cwd = root })
-  else
-    require("telescope.builtin").live_grep()
-  end
+	local root = string.gsub(vim.fn.system("git rev-parse --show-toplevel"), "\n", "")
+	if vim.v.shell_error == 0 then
+		require("telescope.builtin").live_grep({ cwd = root })
+	else
+		require("telescope.builtin").live_grep()
+	end
 end
 
 return {
 
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = "master",
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "master",
 
-    dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = { "nvim-lua/plenary.nvim" },
 
-    config = function()
-      local telescope = require('telescope')
-      local builtin = require('telescope.builtin')
-      local actions = require('telescope.actions')
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
 
-      --
-      -- Plugin Setup
-      --
+			--
+			-- Plugin Setup
+			--
 
-      telescope.setup({
-        defaults = {
-          path_display = {
-            "filename_first"
-          },
+			telescope.setup({
+				defaults = {
+					path_display = {
+						"filename_first",
+					},
 
-          -- key maps
-          mappings = {
-            i = { -- insert mode
-              ["<C-k>"] = actions.move_selection_previous, -- move to prev
-              ["<C-j>"] = actions.move_selection_next, -- move to next
-              ["ĸ"] = actions.move_selection_previous, -- move to prev
-              ["ʒ"] = actions.move_selection_next, -- move to next
-            }
-          }
+					-- key maps
+					mappings = {
+						i = { -- insert mode
+							["<C-k>"] = actions.move_selection_previous, -- move to prev
+							["<C-j>"] = actions.move_selection_next, -- move to next
+							["ĸ"] = actions.move_selection_previous, -- move to prev
+							["ʒ"] = actions.move_selection_next, -- move to next
+						},
+					},
+				}, -- defaults
+			})
 
-        } -- defaults
-      })
+			--
+			-- Key Bindings
+			--
 
-      --
-      -- Key Bindings
-      --
-
-      vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
-      vim.keymap.set("n", "gr", builtin.lsp_references, {})
-      vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
-      vim.keymap.set("n", "ð", builtin.lsp_definitions, {})
-
-      vim.keymap.set("n", "đ", builtin.buffers, {})
-      vim.keymap.set("n", "<C-4>", my_live_grep, {})
-      vim.keymap.set("n", "<C-7>", builtin.oldfiles, {})
-      vim.keymap.set("n", "<C-8>", builtin.buffers, {})
-      vim.keymap.set("n", "<C-9>", builtin.git_files, {})
-      vim.keymap.set("n", "ŋ", builtin.git_files, {})
-      vim.keymap.set("n", "<C-ß>", builtin.lsp_dynamic_workspace_symbols, {})
-      vim.keymap.set("n", "<M-o>", builtin.jumplist, {})
-
-      vim.keymap.set("n", "<M-C-S-F>", ":lua require('telescope.builtin').live_grep({ cwd = '~/Software' })<CR>")
-      vim.keymap.set("n", "<M-F>", ":lua require('telescope.builtin').find_files({ cwd = '~/' })<CR>")
-      vim.keymap.set("n", "<M-f>", ":lua require('telescope.builtin').find_files({ cwd = '~/Software' })<CR>")
-      vim.keymap.set("n", "<M-C-F>", builtin.current_buffer_fuzzy_find, {})
-
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
-
-      vim.keymap.set(
-        "n",
-        "ſ",
-        "<cmd>:Telescope lsp_dynamic_workspace_symbols<CR>"
-      )
-    end
-
-  }
-
+			-- vim.keymap.set("n", "gt", builtin.lsp_type_definitions, {})
+			-- vim.keymap.set("n", "gr", builtin.lsp_references, {})
+			-- vim.keymap.set("n", "gd", builtin.lsp_definitions, {})
+			-- vim.keymap.set("n", "ð", builtin.lsp_definitions, {})
+			--
+			-- vim.keymap.set("n", "đ", builtin.buffers, {})
+			-- vim.keymap.set("n", "<C-4>", my_live_grep, {})
+			-- vim.keymap.set("n", "<C-7>", builtin.oldfiles, {})
+			-- vim.keymap.set("n", "<C-8>", builtin.buffers, {})
+			-- vim.keymap.set("n", "<C-9>", builtin.git_files, {})
+			-- vim.keymap.set("n", "ŋ", builtin.git_files, {})
+			-- vim.keymap.set("n", "<C-ß>", builtin.lsp_dynamic_workspace_symbols, {})
+			-- vim.keymap.set("n", "<M-o>", builtin.jumplist, {})
+			--
+			-- vim.keymap.set("n", "<M-C-S-F>", ":lua require('telescope.builtin').live_grep({ cwd = '~/Software' })<CR>")
+			-- vim.keymap.set("n", "<M-F>", ":lua require('telescope.builtin').find_files({ cwd = '~/' })<CR>")
+			-- vim.keymap.set("n", "<M-f>", ":lua require('telescope.builtin').find_files({ cwd = '~/Software' })<CR>")
+			-- vim.keymap.set("n", "<M-C-F>", builtin.current_buffer_fuzzy_find, {})
+			--
+			-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			-- vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+			-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+			-- vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
+			--
+			-- vim.keymap.set(
+			--   "n",
+			--   "ſ",
+			--   "<cmd>:Telescope lsp_dynamic_workspace_symbols<CR>"
+			-- )
+		end,
+	},
 }
