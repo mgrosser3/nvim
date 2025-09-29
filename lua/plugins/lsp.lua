f12999ea2fbef4055671e015d1e58505586bf40f
Martin Großer <grosser.martin@hotmail.de>
2025-09-29 21:08:02 +0200

    refactor: update LSP configuration

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,18 +5,6 @@
 
 return {
 
-  -- https://github.com/VonHeikemen/lsp-zero.nvim
-  -- --------------------------------------------
-  -- Collection of functions that will help you setup Neovim's LSP client,
-  -- so you can get IDE-like features with minimum effort.
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
-    event = "VeryLazy",
-    lazy = true,
-    config = false
-  },
-
   -- https://github.com/williamboman/mason.nvim
   -- ------------------------------------------
   -- Plugin 'mason.vim' is a package manager for Neovim
@@ -39,74 +27,87 @@ return {
     end
   },
 
-  -- https://github.com/neovim/nvim-lspconfig
-  -- ----------------------------------------
-  -- The plugin nvim-lspconfig provides defauit LSP client configurations
-  -- for a lot of different LSP servers.
+  -- https://github.com/williamboman/mason-lspconfig.nvim
+  -- ----------------------------------------------------
+  -- The plugin mason-lspconfig closes some gaps between mason.nvim
+  -- (see mason.lua) and nvim-lspconfig.
   {
-    'neovim/nvim-lspconfig',
-    event = "VeryLazy",
+    'williamboman/mason-lspconfig.nvim',
+
+    lazy = false,
 
     dependencies = {
 
       { 'williamboman/mason.nvim' },
 
-      -- https://github.com/williamboman/mason-lspconfig.nvim
-      -- ----------------------------------------------------
-      -- The plugin mason-lspconfig closes some gaps between mason.nvim
-      -- (see mason.lua) and nvim-lspconfig.
-      { 'williamboman/mason-lspconfig.nvim' },
+      -- https://github.com/neovim/nvim-lspconfig
+      -- ----------------------------------------
+      -- The plugin nvim-lspconfig provides defauit LSP client configurations
+      -- for a lot of different LSP servers.
+      { 'neovim/nvim-lspconfig' },
 
       -- https://github.com/hrsh7th/cmp-nvim-lsp
       -- ---------------------------------------
       -- Completion source for Neovim's built-in language server client.
       --
       { 'hrsh7th/cmp-nvim-lsp' },
-
     },
 
     config = function()
-      local lsp_zero = require('lsp-zero')
+      local lspconfig = require('lspconfig')
+      local lspconfig_mason = require('mason-lspconfig')
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lsp_attach = function(client, bufnr)
+      local on_attach = function(_, bufnr)
         local opts = { buffer = bufnr, remap = false }
 
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        -- Default Keymaps (since Neovim 0.11)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
+
+        -- Custom Keymaps
+        vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+        vim.keymap.set("n", "]e",
+          function()
+            vim.diagnostic.jump({
+              count = 1,
+              severity = vim.diagnostic.severity.ERROR
+            })
+          end, opts)
       end
 
-      lsp_zero.extend_lspconfig({
-        sign_text = true,
-        lsp_attach = lsp_attach,
-        capabilities = require('cmp_nvim_lsp').default_capabilities()
-      })
+      lspconfig_mason.setup({
+        ensure_installed = { 'lua_ls', 'pyright' },
 
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls' },
         handlers = {
-          -- the default handler applies to every language server
+          -- Default handler applies to every installed language server
           -- without a "custom handler"
           function(server_name)
-            require('lspconfig')[server_name].setup({})
+            lspconfig[server_name].setup({
+              on_attach = on_attach,
+              capabilities = capabilities,
+            })
           end,
 
           -- Java Language Server JDTLS
           -- ---------------------------
           -- It is managed by `nvim-jdtls`, so no configuration is needed here.
           -- For the full Language Server setup, see `ftplugin/java.lua`.
-          jdtls = function()
+          ["jdtls"] = function()
           end,
         }
       })
-    end
+    end,
+
   },
 
   -- https://github.com/hrsh7th/nvim-cmp
@@ -126,25 +127,59 @@ return {
         version = "v2.*",
         -- build = "make install_jsregexp"
       },
+
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' }, -- Wichtig für Snippets
     },
 
     config = function()
       local cmp = require('cmp')
       local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      local luasnip = require('luasnip')
 
       cmp.setup({
+
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
         },
+
         mapping = cmp.mapping.preset.insert({
           ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
           ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<C-Space>'] = cmp.mapping.complete(),
+
+          -- select next completion item OR jump to the next snippet field
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          -- select previous completion item OR jump to the previous snippet field
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jump_prev() then
+              luasnip.jump_prev()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+
         snippet = {
           expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
           end
         }
       })
