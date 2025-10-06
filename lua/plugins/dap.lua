384134cbc2e3299e2d26b660da7936f65602e043
Martin Großer <grosser.martin@hotmail.de>
2025-10-06 17:02:28 +0200

    chore: remove code duplication

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -49,34 +49,6 @@ return {
   },
 
   --
-  -- User Interface
-  --
-
-  {
-    'rcarriga/nvim-dap-ui',
-    event = "VeryLazy",
-
-    config = function()
-      local dapui = require('dapui')
-      local dap = require('dap')
-
-      dapui.setup()
-
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open()
-      end
-
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-      end
-
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-      end
-    end
-  },
-
-  --
   -- Virtual Text
   --
 
