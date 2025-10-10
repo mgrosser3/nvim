41a0cc11d71c31745d7f9d7bda6536cb74f05d2d
Martin Großer <grosser.martin@hotmail.de>
2025-10-10 20:13:19 +0200

    feat: adjust floaterm size and position

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -1,9 +1,12 @@
 return {
-  {
-    'voldikss/vim-floaterm',
-    config = function()
-      vim.keymap.set("n", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
-      vim.keymap.set("t", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
-    end
-  },
+	{
+		"voldikss/vim-floaterm",
+		config = function()
+			vim.g.floaterm_width = 0.95
+			vim.g.floaterm_height = 0.6
+			vim.g.floaterm_position = "bottom"
+			vim.keymap.set("n", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
+			vim.keymap.set("t", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
+		end,
+	},
 }
