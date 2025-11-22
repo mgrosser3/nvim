9a6385d183f6d197d6fbefbf75b7ffb3086dfc7a
Martin Großer <grosser.martin@hotmail.de>
2025-11-22 10:37:57 +0100

    feat: disable auto-selected items in auto-completion

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -157,7 +157,7 @@ return {
 				mapping = cmp.mapping.preset.insert({
 					["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
 					["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 
 					-- select next completion item OR jump to the next snippet field
 					["<Tab>"] = cmp.mapping(function(fallback)
