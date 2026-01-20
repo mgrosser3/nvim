b4900a99c56026d7b2610066d70d46bbbe1123af
Martin Großer <grosser.martin@hotmail.de>
2026-01-20 18:11:20 +0100

    feat: increase floaterm size and set position to center

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -2,9 +2,9 @@ return {
 	{
 		"voldikss/vim-floaterm",
 		config = function()
-			vim.g.floaterm_width = 0.90
-			vim.g.floaterm_height = 0.4
-			vim.g.floaterm_position = "bottom"
+			vim.g.floaterm_width = 0.8
+			vim.g.floaterm_height = 0.8
+			vim.g.floaterm_position = "center"
 			vim.keymap.set("n", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
 			vim.keymap.set("t", "<leader>tt", "<cmd>:FloatermToggle<CR>", { desc = "Toggle Float Terminal" })
 		end,
