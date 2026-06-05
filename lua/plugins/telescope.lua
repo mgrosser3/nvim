33de5dffbd14634fdde6ef3dcaa73528456a27fb
Martin Großer <grosser.martin@hotmail.de>
2026-06-05 21:12:40 +0200

    feat(plugins): remove plugin nvim-treesitter and update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,45 @@
 return {
 
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = "0.1.x",
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
 
-    dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			local telescope = require("telescope")
+			local builtin = require("telescope.builtin")
+			local actions = require("telescope.actions")
 
-    config = function()
-      local telescope = require('telescope')
-      local builtin = require('telescope.builtin')
-      local actions = require('telescope.actions')
+			--
+			-- Plugin Setup
+			--
 
-      --
-      -- Plugin Setup
-      --
+			telescope.setup({
+				defaults = {
 
-      telescope.setup({
-        defaults = {
+					-- key maps
+					mappings = {
+						i = { -- insert mode
+							["<C-k>"] = actions.move_selection_previous, -- move to prev
+							["<C-j>"] = actions.move_selection_next, -- move to next
+						},
+					},
+				}, -- defaults
+			})
 
-          -- key maps
-          mappings = {
-            i = {                                          -- insert mode
-              ["<C-k>"] = actions.move_selection_previous, -- move to prev
-              ["<C-j>"] = actions.move_selection_next,     -- move to next
-            }
-          }
+			--
+			-- Key Bindings
+			--
 
-        } -- defaults
-      })
-
-      --
-      -- Key Bindings
-      --
-
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "files" })
-      vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "git files" })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "buffers" })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "help tags" })
-      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = "grep string" })
-      vim.keymap.set('n', '<leader>fd', builtin.live_grep, { desc = "live grep" })
-      vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = "registers" })
-    end
-
-  }
-
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "files" })
+			vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "git files" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "help tags" })
+			vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "grep string" })
+			vim.keymap.set("n", "<leader>fd", builtin.live_grep, { desc = "live grep" })
+			vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "registers" })
+		end,
+	},
 }
